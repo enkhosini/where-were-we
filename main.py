10a0896b364722d0df1c27d56864f3ac3f37c181
Sobonga <smdlovu1@gmail.com>
2026-07-13 00:52:16 +0200

    before the great intergration

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 #using it to do the preprocessing of the text input of the chat messages
-import yake
-import requests
 import json
-import time
+import pytz
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import requests
+import yake
 import matplotlib.pyplot as plt
-from clock import scroll, flat
+from RealtimeSTT import AudioToTextRecorder
+# import voice_to_speech as vts
 
-text = "Sources tell us that Google is acquiring Kaggle, a platform that hosts data science and machine learning competitions. Details about the transaction remain somewhat vague, but given that Google is hosting its Cloud Next conference in San Francisco this week, the official announcement could come as early as tomorrow. Reached by phone, Kaggle co-founder CEO Anthony Goldbloom declined to deny that the acquisition is happening. Google itself declined 'to comment on rumors'. Kaggle, which has about half a million data scientists on its platform, was founded by Goldbloom  and Ben Hamner in 2010. The service got an early start and even though it has a few competitors like DrivenData, TopCoder and HackerRank, it has managed to stay well ahead of them by focusing on its specific niche. The service is basically the de facto home for running data science and machine learning competitions. With Kaggle, Google is buying one of the largest and most active communities for data scientists - and with that, it will get increased mindshare in this community, too (though it already has plenty of that thanks to Tensorflow and other projects). Kaggle has a bit of a history with Google, too, but that's pretty recent. Earlier this month, Google and Kaggle teamed up to host a $100,000 machine learning competition around classifying YouTube videos. That competition had some deep integrations with the Google Cloud Platform, too. Our understanding is that Google will keep the service running - likely under its current name. While the acquisition is probably more about Kaggle's community than technology, Kaggle did build some interesting tools for hosting its competition and 'kernels', too. On Kaggle, kernels are basically the source code for analyzing data sets and developers can share this code on the platform (the company previously called them 'scripts'). Like similar competition-centric sites, Kaggle also runs a job board, too. It's unclear what Google will do with that part of the service. According to Crunchbase, Kaggle raised $12.5 million (though PitchBook says it's $12.75) since its   launch in 2010. Investors in Kaggle include Index Ventures, SV Angel, Max Levchin, Naval Ravikant, Google chief economist Hal Varian, Khosla Ventures and Yuri Milner "
+tz = pytz.timezone('Africa/Johannesburg')
 
-# text = ' Using a cloud service, rather than running comparable software yourself, essentially outsources the operation of that software to the cloud provider. There are good arguments for and against cloud services. Cloud providers claim that using their services saves you time and money, and allows you to move faster compared to setting up your own infrastructure.'
-# text = 'Cloud providers claim that using their services saves you time and money, and allows you to move faster compared to setting up your own infrastructure'
-#i want to implement the cleaning myself butonly a bit later on
+text = ''
+
+def process_text(text):
+    print(text)
+
+# maybe use the one shot to monologue and then the machine can try to predict what what the user is talking about 
+def start_transcription():
+    recorder = AudioToTextRecorder(
+        transcription_engine="whisper_cpp",
+        enable_realtime_transcription=True,
+        model="tiny.en",
+        device="cpu",
+        beam_size=1,
+    )
+
+    try:
+        while True:
+            recorder.text(process_text)
+    except KeyboardInterrupt:
+        print("Recording stopped by User...")
+        exit(0)
 
 #find a better more general keyword extractor (?)
 # With custom parameters
@@ -21,18 +42,20 @@ custom_kw_extractor = yake.KeywordExtractor(
     dedupLim=0.9,          # deduplication threshold
     dedupFunc='seqm',      # deduplication function
     windowsSize=1,         # context window
-    top=2,                 # number of keywords to extract
+    top=2,                # number of keywords to extract
     features=None,         # custom features
     stopwords=None
 )
-#lower score means more importantrelevent a keyword is
 
+#lower score means more importantrelevent a keyword is
 keywords = custom_kw_extractor.extract_keywords(text)
+
 keyword_list = ''
 max_page_suggestions = 3
 
 for word in keywords:
     keyword_list += (word[0]) + ','
+    # print(word[0])
 
 #find a better API
 url = 'https://en.wikipedia.org/w/rest.php/v1/search/page'
@@ -43,8 +66,15 @@ params = {
     'q': keyword_list,
     'limit': max_page_suggestions
 }
-
 response = requests.get(url, headers=headers, params=params)
+
+
+requestlink = response.url
+
+# response.json() turns the recieved response into a python DS
+data = response.json()
+
+#dump turns python DS into a json string
 requestlink = response.url
 
 # response.json() turns the recieved response into a python DS
@@ -54,17 +84,53 @@ data = response.json()
 results_json = json.dumps(data, indent=2)
 # results_python = json.load(data)
 
+
 print(requestlink)
 # print(results_json)
 
+results_json = json.dumps(data, indent=2)
+# results_python = json.load(data)
+#.split('.')[0] is used to remove the extra precision points in the time stamp 
+df = pd.DataFrame()
+
+def timestamp_now():
+    return pd.to_datetime(pd.Timestamp.now('Africa/Johannesburg'))
 # dataframe format 
-topic_timeline = {
-    'datetime': [],
-    'Main Topic':[],
-    'Main Topic description':[],
-    'Second Topic':[],
-    'Second Topic description': []
+entry_series = {
+    'timestamp':                [timestamp_now()],
+    'Main Topic':               [data['pages'][0]['title']],
+    'Main Topic description':   [data['pages'][0]['description']],
+    'Second Topic':             [data['pages'][1]['title']],
+    'Second Topic description': [data['pages'][1]['description']]
     }
 
-pd.DataFrame(topic_timeline)
-print(topic_timeline)
+entry_series = pd.DataFrame(entry_series)
+
+entry_series['Time'] = entry_series['timestamp'].dt.strftime("%H:%M:%S")
+df = pd.concat([df, entry_series], ignore_index=True)
+
+topic_timeline = df.copy()
+topic_timeline["Level"] = [np.random.randint(-6,-2) if (i%2)==0 else np.random.randint(2,6) for i in range(len(topic_timeline))]
+
+fig, ax = plt.subplots(figsize=(18,9))
+
+# here we are plotting a straight black line with the df['Time] values and the data points (with shape "-o" and in black) will be placed in their positions
+# [0,]* len(df) is the y values for the graph(as a list), where we are telling it to put all the markers at y=0 for the length of the straight line
+ax.plot(topic_timeline['Time'], [0,]* len(df), "-o", color="black", markerfacecolor="white")
+
+ax.set_ylim(-7,7)
+
+for idx in range(len(topic_timeline)):
+    time, topic, level = topic_timeline["Time"][idx], topic_timeline["Main Topic"][idx], topic_timeline["Level"][idx]
+    ax.annotate(topic, xy=(time, 0.1 if level>0 else -0.1),xytext=(time, level),
+                arrowprops=dict(arrowstyle="-",color="red", linewidth=0.8),ha="center"
+               );
+
+ax.spines[["left", "top", "right", "bottom"]].set_visible(False)
+ax.spines[["bottom"]].set_position(("axes", 0.5))
+ax.yaxis.set_visible(False)
+ax.set_title("Current conversation timeline", pad=10, loc="center", fontsize=25, fontweight="bold")
+plt.show()
+
+if __name__ == "__main__":
+    capture_audio()
--- voice_to_speech.py
+++ voice_to_speech.py
@@ -19,32 +19,3 @@ def start_transcription():
     except KeyboardInterrupt:
         print("Recording stopped by User...")
         exit(0)
-
-# if __name__ == "__main__":
-#     recorder = AudioToTextRecorder(
-#     transcription_engine="whisper_cpp",
-#     model="tiny.en",
-#     device="cpu",
-#     beam_size=5,
-#     transcription_engine_options={
-#         "model": {
-#             "n_threads": 8,
-#             "redirect_whispercpp_logs_to": None,
-#         },
-#     },
-#     enable_realtime_transcription=True,
-#     realtime_transcription_engine="whisper_cpp",
-#     realtime_model_type="tiny.en",
-#     beam_size_realtime=1,
-#     realtime_processing_pause=0.15,
-#     realtime_transcription_engine_options={
-#         "model": {
-#             "n_threads": 8,
-#             "redirect_whispercpp_logs_to": None,
-#         },
-#         "transcribe": {
-#             "single_segment": True,
-#             "no_context": True,
-#             "print_timestamps": False,
-#         },
-#     },)
